4d127f318a551ce343cc07077e1f04f10cd3e334
Colin Gillette <cgillette@mfncuso.net>
2019-01-18 11:59:23 -0600

    moving workspace

--- javascript/problem1.js
+++ javascript/problem1.js
@@ -1,3 +1,11 @@
 const array = [7, 18, 10, 4];
-const target = 17;
+const k = 17;
 
+$value = test_target(array, k);
+
+function test_target(array, k)
+{
+    array.forEach(element => {
+        console.log(element);
+    });
+}
