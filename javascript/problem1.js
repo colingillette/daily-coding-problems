662e75ae6aaa54d6835367818ee25482c2aeb7ff
Colin Gillette <colingillette35@gmail.com>
2019-01-18 20:24:39 -0600

    revolve problem1

--- javascript/problem1.js
+++ javascript/problem1.js
@@ -1,11 +1,28 @@
-const array = [7, 18, 10, 4];
-const k = 17;
+// Mostly solved. Issues occur when array has two of the same values, and k is meant to be the sum of those two values.
 
-$value = test_target(array, k);
+const array = [7, 12, 8, 4];
+const k = 16;
+
+console.log(test_target(array, k));
 
 function test_target(array, k)
 {
-    array.forEach(element => {
-        console.log(element);
-    });
+    for (let i = 0; i < array.length; i++)
+    {
+        temp = array.slice(i, i + 1);
+        mover = temp[0];
+
+        for (let j = 0; j < array.length; j++)
+        {
+            if (mover !== array[j])
+            {
+                if (mover + array[j] === k)
+                {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
 }
